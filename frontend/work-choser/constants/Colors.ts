99a04fa57895d72ae05e3d112bcb2e2346fe33ab
Maciej Nowakowski <maciejnowakowski@MacBook-Pro-Maciej.local>
2025-04-18 16:48:38 +0200

    hook test

--- frontend/work-choser/constants/Colors.ts
+++ frontend/work-choser/constants/Colors.ts
@@ -6,7 +6,7 @@
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 export const firstColor = '#808080';
-nowy blad aaaaaaaa
+nowy blad aaaaaaaaa
 
 export const Colors = {
     light: {
