ba1cc46af1a0b524b23052b0d61e6df61c5b8a0f
Maciej Nowakowski <maciejnowakowski@MacBook-Pro-Maciej.local>
2025-04-18 23:01:52 +0200

    test

--- frontend/work-choser/app/_layout.tsx
+++ frontend/work-choser/app/_layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout() {
     if (!loaded) {
         return null;
     }
-
+    // dupa xdxd
     return (
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <Stack>
