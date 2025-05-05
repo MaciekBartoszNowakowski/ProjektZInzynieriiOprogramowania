e44ac91882324b9c3ae665ffea6e863479c11336
Kinga Bunkowska <tcx7@o2.pl>
2025-05-05 04:00:31 +0200

    Test sorting

--- backend/common/tests/test_search_service.py
+++ backend/common/tests/test_search_service.py
@@ -11,48 +11,56 @@ class TestSearchService(TestCase):
 
         self.student_1 = User.objects.create_user(
             username="adam",
+            first_name="Adam",
             role=Role.STUDENT,
             department = self.department_1,
         )
 
         self.student_2 = User.objects.create_user(
             username="Benedykt",
+            first_name="Benedykt",
             role=Role.STUDENT,
             department = self.department_1,
         ) 
 
         self.student_3 = User.objects.create_user(
-            username="Czesław",
+            username="Czesio",
+            first_name="Czesław",
             role=Role.STUDENT,
             department = self.department_2,
         )
 
         self.student_4 = User.objects.create_user(
             username="Dominika",
+            first_name="Dominika",
             role=Role.STUDENT,
             department = self.department_3,
         )
 
         self.supervisor_1 = User.objects.create_user(
             username="Zygmunt",
+            first_name="Zygmunt",
             role=Role.SUPERVISOR,
             department = self.department_1,
         )
 
         self.supervisor_2 = User.objects.create_user(
             username="Włodzimierz",
+            first_name="Włodzimierz",
             role=Role.SUPERVISOR,
             department = self.department_2,
         )
 
         self.supervisor_3 = User.objects.create_user(
-            username="Urszula",
+            username="Ula",
+            first_name="Urszula",
             role=Role.SUPERVISOR,
             department = self.department_3,
         )
         
         self.supervisor_4 = User.objects.create_user(
-            username="Tadeusz",
+            username="Tadek",
+            first_name="Tadeusz",
             role=Role.SUPERVISOR,
             department = self.department_3,
         )
@@ -66,3 +74,24 @@ class TestSearchService(TestCase):
         self.assertIn(self.student_1, results)
         self.assertIn(self.student_2, results)
         self.assertIn(self.supervisor_1, results)
+
+    def test_sorting(self):
+        results = self.search_service.search_user(sort_by=["department", "username"], orders=["asc", "desc"], limit=1000)
+
+        expected_order = [
+            self.supervisor_1, 
+            self.student_2,
+            self.student_1,
+            self.supervisor_2,
+            self.student_3,
+            self.supervisor_3,
+            self.supervisor_4,
+            self.student_4,
+            ]
+
+        self.assertEqual(results.count(), 8)
+        print(results)
+
+        for i, (expected, recieved) in enumerate(zip(expected_order, results)):
+            self.assertEqual(expected, recieved, f"Problem with {i}th element")
+
