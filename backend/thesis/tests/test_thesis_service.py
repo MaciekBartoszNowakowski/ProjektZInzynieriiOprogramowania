d2d32ac76f26dad7c529b27d0cefad11a407033e
Wojciech <wojmichaluk@student.agh.edu.pl>
2025-05-10 18:35:21 +0200

    Added exception class to thesis service and included one more error in endpoints documentation; added thesis service tests

--- backend/thesis/tests/test_thesis_service.py
+++ backend/thesis/tests/test_thesis_service.py
@@ -4,12 +4,8 @@ from users.models import User, Role, AcademicTitle
 from thesis.models import Thesis, ThesisStatus, ThesisType
 from thesis.services.thesis_service import *
 
+
 class TestThesisService(TestCase):
-    pass
-
-    """
-    old tests, will be updated (hopefully soon)
-
     def setUp(self):
         self.department_1 = Department.objects.create(name="Wydział A")
         self.department_2 = Department.objects.create(name="Wydział B")
@@ -70,28 +66,54 @@ class TestThesisService(TestCase):
         self.supervisor_4.tags.add(tag_ml)
         self.supervisor_4.tags.add(tag_math)
 
+        self.non_supervisor = User.objects.create_user(
+            username="NaPewno",
+            first_name="NieJestem",
+            last_name="Promotorem",
+            academic_title=AcademicTitle.ENGINEER,
+            role=Role.STUDENT,
+            department = self.department_1,
+        )
+
         self.thesis_service = ThesisService()
+
 
     def test_adding_thesis_invalid_supervisor_id(self):
         with self.assertRaises(InvalidSupervisorIdException):
             self.thesis_service.add_new_thesis(
-                supervisor_id=self.supervisor_1.pk +\
-                    self.supervisor_2.pk +\
-                    self.supervisor_3.pk +\
-                    self.supervisor_4.pk,
-                thesis_type=ThesisType.ENGINEERING,
-                name="Wizualizacja przepływu gradientów w sieci neuronowej",
-                max_students=3
+                supervisor=self.non_supervisor,
+                validated_data = {
+                    "thesis_type": ThesisType.ENGINEERING,
+                    "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                    "max_students": 3
+                }
             )
+
 
     def test_adding_thesis_invalid_type(self):
         with self.assertRaises(InvalidThesisTypeException):
             self.thesis_service.add_new_thesis(
-                supervisor_id=self.supervisor_1.pk,
-                thesis_type="HABILITATION",
-                name="Bardzo poważna praca",
-                max_students=10
+                supervisor=self.supervisor_1,
+                validated_data = {
+                    "thesis_type": "HABILITATION",
+                    "name": "Bardzo poważna praca",
+                    "max_students": 10
+                }
             )
+
+
+    def test_adding_thesis_no_required_title(self):
+        with self.assertRaises(SupervisorTitleRequiredException):
+            self.thesis_service.add_new_thesis(
+                supervisor=self.supervisor_4,
+                validated_data = {
+                    "thesis_type": ThesisType.DOCTOR,
+                    "name": "Struktury chemiczne substancji opisane w języku Java",
+                    "max_students": 1,
+                    "language": "Polish"
+                }
+            )
+
 
     def test_adding_thesis_limit_exceeded(self):
         with self.assertRaises(ThesisTypeLimitExceededException):
@@ -99,58 +121,192 @@ class TestThesisService(TestCase):
             self.supervisor_2.bacherol_limit = 0
 
             self.thesis_service.add_new_thesis(
-                supervisor_id=self.supervisor_2.pk,
-                thesis_type=ThesisType.BACHELOR,
-                name="Struktury chemiczne substancji opisane w języku Java",
-                max_students=1,
-                language="Polish"
+                supervisor=self.supervisor_2,
+                validated_data = {
+                    "thesis_type": ThesisType.BACHELOR,
+                    "name": "Poważna praca",
+                    "max_students": 5,
+                    "language": "Francais"
+                }
             )
+
+            # dajmy Panu Włodzimierzowi z powrotem gotować <3
+            self.supervisor_2.bacherol_limit = 1
+
 
     def test_adding_thesis_nonpositive_max_students(self):
         with self.assertRaises(NonPositiveStudentsLimitException):
             self.thesis_service.add_new_thesis(
-                supervisor_id=self.supervisor_4.pk,
-                thesis_type=ThesisType.ENGINEERING,
-                name="Wizualizacja przepływu gradientów w sieci neuronowej",
-                max_students=-3
+                supervisor=self.supervisor_4,
+                validated_data = {
+                    "thesis_type": ThesisType.ENGINEERING,
+                    "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                    "max_students": -3
+                }
             )
 
+
     def test_adding_thesis(self):
-        # zmiana limitu dla dr. Włodzimierza
-        self.supervisor_2.bacherol_limit = 1
-
-        self.thesis_service.add_new_thesis(
-            supervisor_id=self.supervisor_4.pk,
-            thesis_type=ThesisType.ENGINEERING,
-            name="Wizualizacja przepływu gradientów w sieci neuronowej",
-            max_students=3
+        self.thesis_1 = self.thesis_service.add_new_thesis(
+            supervisor=self.supervisor_4,
+            validated_data = {
+                "thesis_type": ThesisType.ENGINEERING,
+                "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                "max_students": 3
+            }
         )
 
-        self.thesis_service.add_new_thesis(
-            supervisor_id=self.supervisor_2.pk,
-            thesis_type=ThesisType.BACHELOR,
-            name="Struktury chemiczne substancji opisane w języku Java",
-            max_students=1,
-            language="Polish"
+        self.thesis_2 = self.thesis_service.add_new_thesis(
+            supervisor=self.supervisor_2,
+            validated_data = {
+                "thesis_type": ThesisType.BACHELOR,
+                "name": "Struktury chemiczne substancji opisane w języku Java",
+                "max_students": 1,
+                "language": "Polish"
+            }
         )
 
-        self.thesis_service.add_new_thesis(
-            supervisor_id=self.supervisor_1.pk,
-            thesis_type=ThesisType.MASTER,
-            name="Przechowywanie skompresowanych gradientów i zależności matematycznych w systemie bazodanowym",
-            max_students=2
+        self.thesis_3 = self.thesis_service.add_new_thesis(
+            supervisor=self.supervisor_1,
+            validated_data = {
+                "thesis_type": ThesisType.MASTER,
+                "name": "Przechowywanie skompresowanych gradientów i zależności matematycznych w systemie bazodanowym",
+                "max_students": 2
+            }
         )
 
-        self.thesis_service.add_new_thesis(
-            supervisor_id=self.supervisor_3.pk,
-            thesis_type=ThesisType.DOCTOR,
-            name="Zastosowanie algorytmów grafowych w bioinformatyce i leczeniu nowotworów",
-            language="Spanish"
+        self.thesis_4 = self.thesis_service.add_new_thesis(
+            supervisor=self.supervisor_3,
+            validated_data = {
+                "thesis_type": ThesisType.DOCTOR,
+                "name": "Zastosowanie algorytmów grafowych w bioinformatyce i leczeniu nowotworów",
+                "language": "Spanish"
+            }   
         )
 
         self.assertEqual(Thesis.objects.all().count(), 4)
 
-    def test_getting_all_available_theses(self):
-        available_theses = self.thesis_service.get_available_theses()
-        self.assertEqual(available_theses.count(), 4)
-    """
+
+    def test_update_thesis_invalid_supervisor_id(self):
+        with self.assertRaises(InvalidSupervisorIdException):
+            self.thesis_service.update_thesis(
+                supervisor=self.non_supervisor,
+                thesis_pk=self.thesis_1.pk,
+                validated_data = {
+                    "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                    "status": ThesisStatus.APP_OPEN
+                }
+            )
+
+
+    def test_update_thesis_invalid_thesis_id(self):
+        with self.assertRaises(InvalidThesisIdException):
+            self.thesis_service.update_thesis(
+                supervisor=self.supervisor_4,
+                thesis_pk=-1, # id są normalnie większe niż 0 (?)
+                validated_data = {
+                    "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                    "status": ThesisStatus.APP_OPEN
+                }
+            )
+
+
+    def test_update_thesis_invalid_thesis_id_for_supervisor(self):
+        with self.assertRaises(InvalidThesisIdException):
+            self.thesis_service.update_thesis(
+                supervisor=self.supervisor_2,
+                thesis_pk=self.thesis_1.pk,
+                validated_data = {
+                    "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                    "status": ThesisStatus.APP_OPEN
+                }
+            )
+
+
+    def test_update_thesis_invalid_status(self):
+        with self.assertRaises(InvalidThesisStatusException):
+            self.thesis_service.add_new_thesis(
+                supervisor=self.supervisor_1,
+                thesis_pk=self.thesis_3.pk,
+                validated_data = {
+                    "name": "Bardzo poważna praca",
+                    "status": "Nie ma proszę się rozejść"
+                }
+            )
+
+
+    def test_update_thesis_nonpositive_max_students(self):
+        with self.assertRaises(NonPositiveStudentsLimitException):
+            self.thesis_service.update_thesis(
+                supervisor=self.supervisor_4,
+                thesis_pk=self.thesis_1.pk,
+                validated_data = {
+                    "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                    "status": ThesisStatus.APP_CLOSED,
+                    "max_students": -3
+                }
+            )
+
+
+    def test_update_thesis(self):
+        self.thesis_service.update_thesis(
+            supervisor=self.supervisor_4,
+            thesis_pk=self.thesis_1.pk,
+            validated_data = {
+                "name": "Wizualizacja przepływu gradientów w sieci neuronowej",
+                "status": ThesisStatus.APP_CLOSED
+            }
+        )
+
+        self.assertEqual(self.thesis_1.status, ThesisStatus.APP_CLOSED)
+        self.assertEqual(Thesis.objects.all().count(), 4)
+
+
+    def test_delete_thesis_invalid_supervisor_id(self):
+        with self.assertRaises(InvalidSupervisorIdException):
+            self.thesis_service.delete_thesis(
+                supervisor=self.non_supervisor,
+                thesis_pk=self.thesis_1.pk
+            )
+
+
+    def test_delete_thesis_invalid_thesis_id(self):
+        with self.assertRaises(InvalidThesisIdException):
+            self.thesis_service.delete_thesis(
+                supervisor=self.supervisor_4,
+                thesis_pk=-1
+            )
+
+
+    def test_delete_thesis_invalid_thesis_id_for_supervisor(self):
+        with self.assertRaises(InvalidThesisIdException):
+            self.thesis_service.delete_thesis(
+                supervisor=self.supervisor_2,
+                thesis_pk=self.thesis_1.pk
+            )
+
+
+    def test_delete_thesis(self):
+        self.thesis_service.delete_thesis(
+            supervisor=self.supervisor_4,
+            thesis_pk=self.thesis_1.pk
+        )
+
+        self.assertEqual(Thesis.objects.all().count(), 3)
+
+
+    def test_get_promotor_theses_invalid_supervisor_id(self):
+        with self.assertRaises(InvalidSupervisorIdException):
+            self.thesis_service.get_promotor_theses(supervisor=self.non_supervisor)
+
+
+    def test_get_promotor_theses(self):
+        p1_theses = self.thesis_service.get_promotor_theses(supervisor=self.supervisor_1)
+        p2_theses = self.thesis_service.get_promotor_theses(supervisor=self.supervisor_2)
+        p3_theses = self.thesis_service.get_promotor_theses(supervisor=self.supervisor_3)
+        p4_theses = self.thesis_service.get_promotor_theses(supervisor=self.supervisor_4)
+
+        self.assertEqual(p1_theses.count(), 1)
+        self.assertEqual(p2_theses.count(), 1)
+        self.assertEqual(p3_theses.count(), 1)
+        self.assertEqual(p4_theses.count(), 0)
